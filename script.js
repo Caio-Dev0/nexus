5a1e8decac32fc9ac76be35f653895c6f6c6e394
Caio Lucas <clucaszrtmain@gmail.com>
2025-11-29 22:10:27 -0300

    feat: Adiciona array de itens para filtragem

---
+++ script.js
@@ -0,0 +1,7 @@
+const inventario = [
+  { id: 1, nome: 'Caderno', preco: 25.50, estoque: 10 },
+  { id: 2, nome: 'Caneta', preco: 3.00, estoque: 50 },
+  { id: 3, nome: 'Mochila', preco: 150.00, estoque: 5 },
+  { id: 4, nome: 'Lápis', preco: 1.50, estoque: 100 },
+  { id: 5, nome: 'Borracha', preco: 2.50, estoque: 20 }
+];
